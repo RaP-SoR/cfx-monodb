0a5975173fff06fdc156ccff95ea4cfda15f0a47
RaPSoR <babarapsor@gmail.com>
2025-12-21 19:24:34 +0100

    Update fxmanifest.lua für CFX und

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
-fx_version 'adamant'
-game 'rdr3'
+fx_version 'cerulean'
+game  {'rdr3' , 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'RaPSoR'
-description 'Typescript Wrapper for RedM'
+description 'MongoDB Typescript Wrapper for CFX'
 version '1.0.0'
 
 server_script 'dist/index.js'
@@ -14,9 +14,9 @@ convar_category 'MongoDB' {
   'Konfiguration für MongoDB-Verbindung',
   {
     { "mongodb_env", "dev", "Umgebung (dev, prod, test)", "CV_STRING" },
-    { "mongodb_dev_url", "mongodb://localhost:27017/redm_dev", "MongoDB-Verbindungsstring für Entwicklung", "CV_STRING" },
-    { "mongodb_prod_url", "mongodb://localhost:27017/redm_prod", "MongoDB-Verbindungsstring für Produktion", "CV_STRING" },
-    { "mongodb_test_url", "mongodb://localhost:27017/redm_test", "MongoDB-Verbindungsstring für Tests", "CV_STRING" },
+    { "mongodb_dev_url", "mongodb://localhost:27017/cfx_dev", "MongoDB-Verbindungsstring für Entwicklung", "CV_STRING" },
+    { "mongodb_prod_url", "mongodb://localhost:27017/cfx_prod", "MongoDB-Verbindungsstring für Produktion", "CV_STRING" },
+    { "mongodb_test_url", "mongodb://localhost:27017/cfx_test", "MongoDB-Verbindungsstring für Tests", "CV_STRING" },
     { "mongodb_timeout", "5000", "Timeout für MongoDB-Verbindung in Millisekunden", "CV_STRING" }
   }
 }
